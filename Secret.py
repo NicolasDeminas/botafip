e85f84ce50fa2e936e28374d09cf52165bbcff2b
Nico <deminas.nicolas@gmail.com>
2020-11-17 09:42:59 -0300

    Borrada informacion que no debia mostrarse

--- Secret.py
+++ Secret.py
@@ -6,9 +6,6 @@ from dotenv import load_dotenv
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
-password = "887Estudio"
-username = "20184719968"
-
 username_food = os.environ['username_food']
 password_food = os.environ['password_food']
 
