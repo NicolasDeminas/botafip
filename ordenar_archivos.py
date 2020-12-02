8e566f80aa577fb9083bf8c8c19d68fecebad5ed
Nico <deminas.nicolas@gmail.com>
2020-12-02 15:12:23 -0300

    modificado para ordenar archivo de comp emitidos ademas de recibidos

---
+++ ordenar_archivos.py
@@ -0,0 +1,23 @@
+import os,datetime, time, pathlib
+import shutil
+
+carpeta_a_ordenar = 'c:/Users/Nicolas/Downloads/'
+mis_comprobantes_recibidos_base='Mis Comprobantes Recibidos - CUIT 30698322337'
+carpeta_comprobantes_recibidos = carpeta_a_ordenar + '/Comprobantes recibidos/'
+mis_comprobantes_emitidos_base='Mis Comprobantes Emitidos - CUIT 30698322337'
+carpeta_comprobantes_emitidos = carpeta_a_ordenar + '/Comprobantes emitidos/'
+
+
+def ordenar_archivos(carpeta_a_ordenar, mis_comprobantes_base, carpeta_destino):
+    directorio = pathlib.Path(carpeta_a_ordenar)
+    for archivo in directorio.iterdir():
+        a = archivo.name.find(mis_comprobantes_base)
+        if a >= 0:
+            fecha_creacion = time.ctime(os.path.getmtime(carpeta_a_ordenar +'/'+ archivo.name))
+            fecha = datetime.datetime.strptime(fecha_creacion, "%a %b %d %H:%M:%S %Y")
+            fecha = (datetime.datetime.strftime(fecha, '%Y-%m-%d-%H%M%S'))
+            comprobante = mis_comprobantes_base + ' - ' + str(fecha) + '.csv'
+            shutil.move(carpeta_a_ordenar + archivo.name, carpeta_destino + comprobante)
+
+ordenar_archivos(carpeta_a_ordenar, mis_comprobantes_recibidos_base, carpeta_comprobantes_recibidos)
+ordenar_archivos(carpeta_a_ordenar,mis_comprobantes_emitidos_base, carpeta_comprobantes_emitidos)
