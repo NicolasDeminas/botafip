ef1794b2b15a16c504d1c738396dcd436bcd0477
Nico <deminas.nicolas@gmail.com>
2020-12-15 15:17:22 -0300

    nueva funcion de ordenar archivos

--- ordenar_archivos.py
+++ ordenar_archivos.py
@@ -1,16 +1,19 @@
 import os,datetime, time, pathlib
 import shutil
 
-carpeta_a_ordenar = 'c:/Users/Nicolas/Downloads/'
-mis_comprobantes_recibidos_base='Mis Comprobantes Recibidos - CUIT 30698322337'
-carpeta_comprobantes_recibidos = carpeta_a_ordenar + '/Comprobantes recibidos/'
-mis_comprobantes_emitidos_base='Mis Comprobantes Emitidos - CUIT 30698322337'
-carpeta_comprobantes_emitidos = carpeta_a_ordenar + '/Comprobantes emitidos/'
+carpeta_downloads = 'c:/Users/Nicolas/Downloads/'
+mis_comprobantes_recibidos_food='Mis Comprobantes Recibidos - CUIT 30698322337'
+carpeta_comprobantes_recibidos = carpeta_downloads + '/Comprobantes recibidos/'
+mis_comprobantes_emitidos_food='Mis Comprobantes Emitidos - CUIT 30698322337'
+carpeta_comprobantes_emitidos = carpeta_downloads + '/Comprobantes emitidos/'
 comprobantes_recibidos_anser = 'Mis Comprobantes Recibidos - CUIT 30689189233'
 comprobantes_emitidos_anser = 'Mis Comprobantes Emitidos - CUIT 30689189233'
-carpeta_comprobantes_recibidos_anser = carpeta_a_ordenar + '/Comprobantes recibidos Anser/'
-carpeta_comprobantes_emitidos_anser = carpeta_a_ordenar + '/Comprobantes emitidos Anser/'
-
+carpeta_comprobantes_recibidos_anser = carpeta_downloads + '/Comprobantes recibidos Anser/'
+carpeta_comprobantes_emitidos_anser = carpeta_downloads + '/Comprobantes emitidos Anser/'
+archivos_acreditaciones = 'Acreditaciones_GO_30698322337'
+carpeta_acreditaciones = 'Archivos acreditaciones sueldos'
+pdfs = '.pdf'
+carpeta_pdfs = 'Pdfs'
 
 
 
@@ -25,7 +28,18 @@ def ordenar_archivos(carpeta_a_ordenar, mis_comprobantes_base, carpeta_destino):
             comprobante = mis_comprobantes_base + ' - ' + str(fecha) + '.csv'
             shutil.move(carpeta_a_ordenar + archivo.name, carpeta_destino + comprobante)
 
-ordenar_archivos(carpeta_a_ordenar, mis_comprobantes_recibidos_base, carpeta_comprobantes_recibidos)
-ordenar_archivos(carpeta_a_ordenar,mis_comprobantes_emitidos_base, carpeta_comprobantes_emitidos)
-ordenar_archivos(carpeta_a_ordenar, comprobantes_recibidos_anser, carpeta_comprobantes_recibidos_anser)
-ordenar_archivos(carpeta_a_ordenar, comprobantes_emitidos_anser, carpeta_comprobantes_emitidos_anser)
+ordenar_archivos(carpeta_downloads, mis_comprobantes_recibidos_food, carpeta_comprobantes_recibidos)
+ordenar_archivos(carpeta_downloads,mis_comprobantes_emitidos_food, carpeta_comprobantes_emitidos)
+ordenar_archivos(carpeta_downloads, comprobantes_recibidos_anser, carpeta_comprobantes_recibidos_anser)
+ordenar_archivos(carpeta_downloads, comprobantes_emitidos_anser, carpeta_comprobantes_emitidos_anser)
+
+
+def ordenarVarios(carpeta_a_ordenar, tipoArchivo, carpetaDestino):
+    directorio = pathlib.Path(carpeta_a_ordenar)
+    for archivo in directorio.iterdir():
+        a = archivo.name.find(tipoArchivo)
+        if a >= 0:
+            shutil.move(carpeta_a_ordenar + archivo.name, carpeta_a_ordenar + carpetaDestino )
+        
+ordenarVarios(carpeta_downloads, archivos_acreditaciones, carpeta_acreditaciones)
+ordenarVarios(carpeta_downloads, pdfs, carpeta_pdfs)
