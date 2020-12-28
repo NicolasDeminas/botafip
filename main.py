945a0ba1de0e685b7a96245cddeec2090ebb1581
Nico <deminas.nicolas@gmail.com>
2020-12-28 12:46:35 -0300

    probando cambios en la GUI

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ from tkinter import Tk, mainloop, COMMAND, Button
 from Afip import comprobantesRecibidosFood, comprobantesEmitidosFood, comprobantesRecibidosAnser,comprobantesEmitidosAnser
 
 root = Tk()
+root.geometry("400x200")
 
 compRecibidosFood = Button(root, text="Comprobantes Recibidos Food", command=comprobantesRecibidosFood)
 compEmitidosFood = Button(root, text="Comprobantes Emitidos Food", command=comprobantesEmitidosFood)
 compRecibidosAnser = Button(root, text="Comprobantes Recibidos Anser", command=comprobantesRecibidosAnser)
 compEmitidosAnser = Button(root, text="Comprobantes Emitidos Anser", command=comprobantesEmitidosAnser)
-compRecibidosFood.pack()
-compEmitidosFood.pack()
-compRecibidosAnser.pack()
-compEmitidosAnser.pack()
+
+compRecibidosFood.grid(row=0, column=0)
+compEmitidosFood.grid(row=1, column=0)
+compRecibidosAnser.grid(row=0, column=1)
+compEmitidosAnser.grid(row=1, column=1)
 
 root.mainloop()
